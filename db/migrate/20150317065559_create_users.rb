835103efeaae3eb0ae34b7add35a8d531c492367
Henrique Carvalho <henrique.o.carvalho@hotmail.com.br>
2015-03-17 15:13:20 -0700

    update Model

--- db/migrate/20150317065559_create_users.rb
+++ db/migrate/20150317065559_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.boolean :admin
-      t.datetime :last_signed_in_at
+      t.string :name, default: ''
+      t.string :email, null: false
+      t.string :password, null: false
+      t.boolean :admin, default: false
+      t.datetime :last_signed_in_at, null: false, default: Time.now
 
       t.timestamps null: false
     end
